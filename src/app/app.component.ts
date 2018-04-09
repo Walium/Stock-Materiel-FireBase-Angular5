f3f7e2f12b672cf1f93169c6bd6126ff75c6029e
Walid Bouanani <walidbouanani1990@gmail.com>
2018-04-09 12:27:49 +0200

    Ajout des Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,7 @@ import {AppareilService} from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-isAuth = false;
- //lastUpdate = new Date();
- lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-
-appareils :any[];
-
-constructor(private appareilService:AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
-  }
-
-  ngOnInit(){
-  this.appareils=this.appareilService.appareils;
-  }
-
-  onAllumer() {
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-  this.appareilService.switchOffAll();
-  }
+constructor(){}
 }
